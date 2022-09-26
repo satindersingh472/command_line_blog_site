cd186c39afc0c92b4afe4545909e803fbc060132
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-09-26 16:34:56 -0600

    finished

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from http import client
+#import database
 import mariadb
+#import credentials from dbcreds file
 import dbcreds
-
+#below function will take the input for username and password from user
+#it will connect to the database and open the cursor
+#execute the stored procedures with given username and password as a arguments
+#the result from the database will get stored inside result 
+#then close the cursor and connection in an order
+#if there is any user associated with the given username and password 
+#the function will return result[0] otherwise none will be returned
 def ask_username_and_password():
     username = input('Please enter your username: ')
     password = input('Please enter your password: ')
@@ -12,23 +19,32 @@ def ask_username_and_password():
     cursor.close()
     conn.close()
     if(len(result) == 1):
+        print('Sign in is successfull')
+        print('Welcome,',result[0][1])
         return result[0]
     elif(len(result) == 0):
         print('Invalid username or password')
         return None
-    
+
+#insert post will take title and content as inputs from user after successfull sign in  
+#it will connect to database and open the cursor and execute the stored procedures
+#after executing it will close the cursor and connection and print the statement
+#and return the function
 def insert_post(argument1):
     insert_title = input('Please add title for the post: ')
     insert_content = input('Please add content for the post: ')
-    print('Blog post added successfully')
     conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
     cursor = conn.cursor()
     cursor.execute('call insert_post(?,?,?)',[argument1, insert_title, insert_content])
     cursor.close()
     conn.close()
+    print('Blog post added successfully')
     return 
 
-
+#all posts will display all the post in the database
+#it will connect to database and open the cursor and execute the stored procedure
+#it will store the data from stored procedures inside the result variable 
+#after getting and storing data it will close the cursor and connection and return the result
 def all_posts():
     conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
     cursor = conn.cursor()
@@ -38,21 +54,28 @@ def all_posts():
     conn.close()
     return result
 
-
+#infinite ask is the function that will have while loop and call the functions base on input
 def infinite_ask():
+    #while loop will continue to work until we break it 
     while(True):
         print('Option 1 : Will display all the blog posts with the title....')
         print('Option 2 : Will let the user sign in with username and password and user can write title and content for blog post....')
         print('Option 3 and greater: Will let the user exit from the function and print a goodbye message....')
         user_input = input('Please select option 1 or 2 or 3: ')
+        #based on inputs the loop will try different conditions and try or except methods will prevent the code
         try:
+            #the user input will get converted to integer value
             user_input = int(user_input)
+            #if user enters 1 then post variable will execute all posts and store the data inside posts itself
             if(user_input == 1):
                 posts = all_posts()
+                #there will be many posts inside posts variable then it will take the help of loop to display all posts
                 for post in posts:
                     print('The title of the post: ', post[0])
                     print('The content of the post: ', post[1])
                     print('------------------------')
+                    #after writing all the posts it will ask user to further choose from 3 options
+                    #based on user input if yes then the while loop will continue otherwise not
                 ask_user = input('Do you want to see options again? (yes/no) ')
                 if ask_user in ['','y','yes']:
                     continue
@@ -62,17 +85,24 @@ def infinite_ask():
                 else:
                     print('Value entered was not valid.So, please proceed further with the options.....enjoy...')
                     continue
+                #if user enters 2 then it will execute the function ask_username_and_password()
             elif(user_input == 2):
                 client_id =  ask_username_and_password()
+                #if it returns something then insert post function will get executed with the argument
+                #containing the client id from the ask_username_and_password()
                 if(client_id):
                     insert_post(client_id[0])
+                    #if user enters integer 0 or less than 0 OR 4 or greater than 
+                    #it will show the message to enter correct value and continue the loop
             elif(user_input <= 0 or user_input >= 4):
                 print('Only values from 1 to 3 will show any output, please try again')
                 continue
+            #if user enters 3 then loop will break and print a goodbye message
             elif(user_input == 3):
                 print('Good bye user have a wonderfull day....')
                 break
-
+                #if user enters any other option than integer while selecting options 1, 2 or 3
+                #then the value error will show the error message
         except ValueError:
             print('You have selected the value which is not included or it is not an integer, please select from the given numbers(1, 2 or 3)')
 
--- backup_file.sql
+++ backup_file.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.6.7-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for osx10.17 (x86_64)
 --
 -- Host: localhost    Database: command_line_blog_site
 -- ------------------------------------------------------
--- Server version	10.6.7-MariaDB-2ubuntu1.1
+-- Server version	10.8.3-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -38,7 +38,10 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES (1,'satinder','2000-01-01','123456'),(2,'sam','2005-01-01','123456'),(3,'nuhaar','2015-01-01','123456');
+INSERT INTO `client` VALUES
+(1,'satinder','2000-01-01','123456'),
+(2,'sam','2005-01-01','123456'),
+(3,'nuhaar','2015-01-01','123456');
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +60,7 @@ CREATE TABLE `post` (
   PRIMARY KEY (`id`),
   KEY `post_FK` (`client_id`),
   CONSTRAINT `post_FK` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,13 +69,29 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'hardwork always pays off, no matter what','hardwork',1),(2,'honesty always wins','honesty',2),(3,'never underestimate the power of time','time',3),(4,'always be a good listener','listener',1),(5,'technology is growing day by day','technology',2),(6,'babies do not let their parents sleep','sleepless',2),(7,'winter is almost there','winter',2),(8,'canada has a cold weather','weather',2),(9,'children are so cute','children',2),(10,'I have finished the assignment','finished',2),(11,'34343','3',2),(12,'I like summers ','summers',2),(13,'hoe much is left','helllo',2);
+INSERT INTO `post` VALUES
+(1,'hardwork always pays off, no matter what','hardwork',1),
+(2,'honesty always wins','honesty',2),
+(3,'never underestimate the power of time','time',3),
+(4,'always be a good listener','listener',1),
+(5,'technology is growing day by day','technology',2),
+(6,'babies do not let their parents sleep','sleepless',2),
+(7,'winter is almost there','winter',2),
+(8,'canada has a cold weather','weather',2),
+(9,'children are so cute','children',2),
+(10,'I have finished the assignment','finished',2),
+(11,'34343','3',2),
+(12,'I like summers ','summers',2),
+(13,'hoe much is left','helllo',2),
+(14,'child care is expensive','childcare',2);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'command_line_blog_site'
 --
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `all_posts` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -80,8 +99,6 @@ UNLOCK TABLES;
 /*!50003 SET character_set_client  = utf8mb4 */ ;
 /*!50003 SET character_set_results = utf8mb4 */ ;
 /*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `all_posts`()
 BEGIN
@@ -93,6 +110,8 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `find_client_id` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -100,12 +119,10 @@ DELIMITER ;
 /*!50003 SET character_set_client  = utf8mb4 */ ;
 /*!50003 SET character_set_results = utf8mb4 */ ;
 /*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `find_client_id`(username_input varchar(100), password_input varchar(100))
 BEGIN
-	select c.id 
+	select c.id, c.username 
 	from client c where c.username = username_input and c.password =  password_input;
 END ;;
 DELIMITER ;
@@ -113,6 +130,8 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `insert_post` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -120,8 +139,6 @@ DELIMITER ;
 /*!50003 SET character_set_client  = utf8mb4 */ ;
 /*!50003 SET character_set_results = utf8mb4 */ ;
 /*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_post`(client_id_input int unsigned, title_input varchar(100), content_input varchar(100))
     MODIFIES SQL DATA
@@ -144,4 +161,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-26 14:21:12
+-- Dump completed on 2022-09-26 16:32:59
